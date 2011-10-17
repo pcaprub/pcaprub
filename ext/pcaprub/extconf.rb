21173825321b08e1edef18b0c61666e7f2b3ab36
shadowbq <shadowbq@gmail.com>
2011-10-17 15:32:02 -0400

    cystek issue #2 closed

--- ext/pcaprub/extconf.rb
+++ ext/pcaprub/extconf.rb
@@ -2,7 +2,7 @@ require 'mkmf'
 extension_name = 'pcaprub'
 
 if /i386-mswin32/ =~ RUBY_PLATFORM
-  pcap_dir        = with_config("pcap-dir", "C:\WpdPack")
+  pcap_dir        = with_config("pcap-dir", "C:\\WpdPack")
   pcap_includedir = with_config("pcap-includedir", pcap_dir + "\\include")
   pcap_libdir     = with_config("pcap-libdir", pcap_dir + "\\lib")
 
