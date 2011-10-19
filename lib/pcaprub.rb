2af0589dc2ed60eb6e0bb54a87a57e754a80b15a
shadowbq <shadowbq@gmail.com>
2011-10-19 11:14:34 -0400

    added basic ruby modules to gem

--- lib/pcaprub.rb
+++ lib/pcaprub.rb
@@ -1,15 +1,6 @@
-require File.expand_path(File.dirname(__FILE__)) + "/pcaprub.so"
-
-module PCAP
-  # The base exception for JSON errors.
-  class PCAPRUBError < StandardError; end
-
-  # This exception is raised, if a parser error occurs.
-  class BindingError < PCAPRUBError; end
-
-  # This exception is raised, if the nesting of parsed datastructures is too
-  # deep.
-  class BPFError < PCAPRUBError; end
-
+module JSON
+  $:.unshift(File.dirname(__FILE__))
+  require 'pcaprub/common'
+  require 'pcaprub/ext'
 end
 
---
+++ lib/pcaprub/common.rb
@@ -0,0 +1,14 @@
+
+module PCAP
+  # The base exception for JSON errors.
+  class PCAPRUBError < StandardError; end
+
+  # This exception is raised, if a parser error occurs.
+  class BindingError < PCAPRUBError; end
+
+  # This exception is raised, if the nesting of parsed datastructures is too
+  # deep.
+  class BPFError < PCAPRUBError; end
+
+end
+
