ebf77515ccfc7591ec9ed044a0a1ef70b40bdc51
shadowbq <shadowbq@gmail.com>
2011-10-19 11:02:19 -0400

    added examples, reworking module

--- lib/pcaprub.rb
+++ lib/pcaprub.rb
@@ -1,1 +1,15 @@
 require File.expand_path(File.dirname(__FILE__)) + "/pcaprub.so"
+
+module PCAP
+  # The base exception for JSON errors.
+  class PCAPRUBError < StandardError; end
+
+  # This exception is raised, if a parser error occurs.
+  class BindingError < PCAPRUBError; end
+
+  # This exception is raised, if the nesting of parsed datastructures is too
+  # deep.
+  class BPFError < PCAPRUBError; end
+
+end
+
