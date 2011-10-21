6116fe7208d5401bcd0eab79d143501a5e7279d8
shadowbq <shadowbq@gmail.com>
2011-10-21 09:24:03 -0400

    Version bump to 0.11.0

---
+++ lib/pcaprub/version.rb
@@ -0,0 +1,18 @@
+module PCAPRUB #:nodoc:
+  
+  module VERSION #:nodoc:
+    
+    MAJOR = 0
+    MINOR = 10
+    TINY = 1
+
+    STRING = [MAJOR, MINOR, TINY].join('.')
+    
+  end
+  class Pcap
+    def self.version 
+      return PCAPRUB::VERSION::STRING
+    end
+  end
+  
+end  
