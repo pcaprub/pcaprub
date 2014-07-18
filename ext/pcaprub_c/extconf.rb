c3a06c039a14c14b0820adfb47e62a7ad33c2c8c
ryanatball <rmelton@ball.com>
2014-07-18 16:21:18 -0600

    Fix building on mingw32

--- ext/pcaprub_c/extconf.rb
+++ ext/pcaprub_c/extconf.rb
@@ -18,7 +18,9 @@ if /i386-mingw32/ =~ RUBY_PLATFORM
   pcap_dir        = with_config("pcap-dir", "C:/WpdPack")
   pcap_includedir = with_config("pcap-includedir", pcap_dir + "/include")
   pcap_libdir     = with_config("pcap-libdir", pcap_dir + "/lib")
-
+  $CFLAGS  = "-DWIN32 -I#{pcap_includedir}"
+  $LDFLAGS = "-L#{pcap_libdir}"
+  
   have_library("wpcap", "pcap_open_live")
   have_library("wpcap", "pcap_setnonblock")
 
