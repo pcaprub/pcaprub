c7611f99df46da5e5a4f84c9edd95bcbb890a35b
Dario Lombardo <lomato@gmail.com>
2015-01-17 23:08:13 +0100

    Fixed version number

--- lib/pcaprub/version.rb
+++ lib/pcaprub/version.rb
@@ -3,8 +3,8 @@ module PCAPRUB #:nodoc:
   module VERSION #:nodoc:
 
     MAJOR = 0
-    MINOR = 13
-    TINY = 0
+    MINOR = 12
+    TINY = 1
 
     STRING = [MAJOR, MINOR, TINY].join('.')
 
@@ -15,4 +15,4 @@ module PCAPRUB #:nodoc:
     end
   end
 
-end  
+end
