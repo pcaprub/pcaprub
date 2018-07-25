2288c9fd942bc33b552fa51a5c4c95d5ba396644
Dario Lombardo <lomato@gmail.com>
2018-07-25 19:41:32 +0200

    Remove more 1.x checks.

--- lib/pcaprub/ext.rb
+++ lib/pcaprub/ext.rb
@@ -1,9 +1,5 @@
 begin
-  if RUBY_VERSION =~ /1.8/
-    require '1.8/pcaprub_c'
-  elsif RUBY_VERSION =~ /1.9/
-    require '1.9/pcaprub_c'
-  elsif RUBY_VERSION =~ /2.0/
+  if RUBY_VERSION =~ /2.0/
     require '2.0/pcaprub_c'
   elsif RUBY_VERSION =~ /2.1/
     require '2.1/pcaprub_c'
