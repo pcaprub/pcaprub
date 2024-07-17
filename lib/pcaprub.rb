7ff0ff503f7949e99746342384678077077a070f
Dean Welch <dean_welch@rapid7.com>
2024-07-17 13:48:05 +0100

    safely load ruby_installer and manually add to path

--- lib/pcaprub.rb
+++ lib/pcaprub.rb
@@ -7,8 +7,13 @@ module PCAPRUB
     # On Windows load npcap first if it exists, will fall back to winpcap if that's installed
     npcap_path = "C:\\Windows\\System32\\Npcap"
     if File.directory?(npcap_path)
-      require 'ruby_installer'
-      RubyInstaller::Runtime.add_dll_directory(npcap_path)
+      begin
+        require 'ruby_installer/runtime'
+        RubyInstaller::Runtime.add_dll_directory(npcap_path)
+      rescue LoadError
+        old_path = ENV['PATH']
+        ENV['PATH'] = "#{npcap_path};#{old_path}"
+      end
     end
   end
 
